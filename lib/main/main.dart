d567f633768d8550fe13c1695a3b90fc9d554ce4
aimer <guhy.6376.uvyt@gmail.com>
2026-06-12 09:57:52 +0800

    初始化 Firebase 串接

--- lib/main/main.dart
+++ lib/main/main.dart
@@ -1,8 +1,17 @@
 import 'package:flutter/material.dart';
-// 因為檔案在 lib/main/ 下，要往上跳一層到 lib，再進入 admin_dashboard
+import 'package:firebase_core/firebase_core.dart'; // 1. 記得引入這個
 import '../admin_dashboard/admin_dashboard_page.dart';
+import 'firebase_options.dart'; // 2. 這是你執行 flutterfire configure 後產生的檔案
 
-void main() {
+void main() async {
+  // 3. 確保 Flutter 引擎啟動完成
+  WidgetsFlutterBinding.ensureInitialized();
+  
+  // 4. 初始化 Firebase
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+
   runApp(const MyApp());
 }
 
