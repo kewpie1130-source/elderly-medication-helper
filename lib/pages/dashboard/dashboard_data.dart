fbb2179c57ef3e77c597babb7ab3c9a27438c86d
Aimer <guhy.6376.uvyt@gmail.com@example.com>
2026-06-17 15:43:51 +0800

    feat: [組員C] 建立 Dashboard 數據模型

---
+++ lib/pages/dashboard/dashboard_data.dart
@@ -0,0 +1,12 @@
+﻿class DashboardAnalytics {
+  final int totalTaken;
+  final int totalMissed;
+  final double adherenceRate;
+
+  DashboardAnalytics({
+    required this.totalTaken,
+    required this.totalMissed,
+  }) : adherenceRate = (totalTaken + totalMissed) == 0 
+        ? 0.0 
+        : (totalTaken / (totalTaken + totalMissed)) * 100;
+}
