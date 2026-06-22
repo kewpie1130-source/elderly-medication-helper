6635533c625d0c68d1899c1a25a3c114b0687a22
mys1050306-cpu <mys1050306@gmail.com>
2026-06-22 13:58:29 +0800

    fix: [邱靖喻] 強制統一所有子模組(含第三方套件)Java/Kotlin版本為17，根本解決版本衝突

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -15,6 +15,11 @@ subprojects {
     val newSubprojectBuildDir: Directory = newBuildDir.dir(project.name)
     project.layout.buildDirectory.value(newSubprojectBuildDir)
 
+    tasks.withType<JavaCompile>().configureEach {
+        sourceCompatibility = "17"
+        targetCompatibility = "17"
+    }
+
     tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile>().configureEach {
         compilerOptions {
             jvmTarget.set(org.jetbrains.kotlin.gradle.dsl.JvmTarget.JVM_17)
